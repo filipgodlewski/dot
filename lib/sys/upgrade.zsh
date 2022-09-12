197863907c546ab4d491a3d69b810dda0ea9bbe7
Filip Godlewski <filip.godlewski@outlook.com>
2022-09-12 22:52:15 +0200

    fix: 🐛 minor changes to sys up

--- lib/sys/upgrade.zsh
+++ lib/sys/upgrade.zsh
@@ -36,8 +36,8 @@ function _dot::sys::upgrade::_hosts {
   (($? == 1)) && {echo "No 'hosts provider' set in $DOTDIR_CONFIG! Can't perform setting up hosts."; return 0}
 
   if [[ $(_dot::submodule::ls --key sys | grep $provider 2> /dev/null) ]]; then
-    local repo_dir="$XDG_DATA_HOME/$(git submodule | grep $provider | cut -d' ' -f3)"
-    venv new --project-path $repo_dir 2> /dev/null
+    local repo_dir="$XDG_DATA_HOME/$(git -C $DOTDIR submodule | grep $provider | cut -d' ' -f3)"
+    venv new --project-path $DOTDIR/$repo_dir --no-link 2> /dev/null
     venv run --name $provider -m pip install -r $repo_dir/requirements.txt
     venv run --name $provider $repo_dir/updateHostsFile.py -e fakenews gambling porn -f -r -a
   fi
