f3c854151aaaf19194f924b14054147014a6e9ec
Filip Godlewski <filip.godlewski@outlook.com>
2022-09-14 11:44:50 +0200

    fix: 🐛 change how to cut path from submodules

--- lib/sys/upgrade.zsh
+++ lib/sys/upgrade.zsh
@@ -2,14 +2,14 @@
 
 function _dot::sys::up { _dot::sys::upgrade "$@" }
 function _dot::sys::upgrade {
-  zparseopts -D -F -K -all=all -npm=npm_pkgs -brew=brew_pkgs -submodules=git_submodules -nvim=nvim_pip -hosts=hosts_file
+  zparseopts -D -F -A opts -all -npm -brew -submodules -nvim -hosts
 
-  (($#all || $#npm_pkgs)) && $0::_npm
-  (($#all || $#brew_pkgs)) && $0::_brew
-  (($#all || $#git_submodules)) && _dot::submodule::up
+  (($#opts[--all] || $#opts[--npm])) && $0::_npm
+  (($#opts[--all] || $#opts[--brew])) && $0::_brew
+  (($#opts[--all] || $#opts[--submodules])) && _dot::submodule::up
   if [[ $+functions[_venv] ]]; then  # external dependency!
-    (($#all || $#nvim_pip)) && {echo ":: Upgrade nvim venv ::"; venv update nvim}
-    (($#all || $#hosts_file)) && $0::_hosts
+    (($#opts[--all] || $#opts[--nvim])) && {echo ":: Upgrade nvim venv ::"; venv update nvim}
+    (($#opts[--all] || $#opts[--hosts])) && $0::_hosts
   fi
 }
 
@@ -37,7 +37,7 @@ function _dot::sys::upgrade::_hosts {
   (($? == 1)) && {echo "No 'hosts provider' set in $DOTDIR_CONFIG! Can't perform setting up hosts."; return 0}
 
   if [[ $(_dot::submodule::ls --key sys | grep $provider 2> /dev/null) ]]; then
-    local repo_dir="$(git -C $DOTDIR submodule | grep $provider | cut -d' ' -f3)"
+    local repo_dir="$(echo ${$(git -C $DOTDIR submodule | grep $provider)##[[:blank:]]} | cut -d' ' -f2)"
     venv new --project-path $DOTDIR/$repo_dir --no-link 2> /dev/null
     venv run --name $provider -m pip install -r $DOTDIR/$repo_dir/requirements.txt
     venv run --name $provider $DOTDIR/$repo_dir/updateHostsFile.py -e fakenews gambling porn -f -r -a
